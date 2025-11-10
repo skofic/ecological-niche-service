98bd89c85792b6ef3763360e170babf0bcb8d4e1
Milko Škofič <milko@skofic.io>
2025-11-10 14:36:51 +0100

    Finalised chorology.
    The chorology service now works also on the server. Bumped version number from 1.0.0 to 1.0.1.

--- api/EcologicalNiche.Node.js
+++ api/EcologicalNiche.Node.js
@@ -51,6 +51,57 @@
     console.log('BODY:', body);
 });
 
+// request Return species chorology 
+(function(callback) {
+    'use strict';
+        
+    const httpTransport = require('http');
+    const responseEncoding = 'utf8';
+    const httpOptions = {
+        hostname: 'localhost',
+        port: '8529',
+        path: '/_db/GeoService/scat/species/chorology?species=Abies%20alba',
+        method: 'GET',
+        headers: {}
+    };
+    httpOptions.headers['User-Agent'] = 'node ' + process.version;
+ 
+    // Paw Store Cookies option is not supported
+
+    const request = httpTransport.request(httpOptions, (res) => {
+        let responseBufs = [];
+        let responseStr = '';
+        
+        res.on('data', (chunk) => {
+            if (Buffer.isBuffer(chunk)) {
+                responseBufs.push(chunk);
+            }
+            else {
+                responseStr = responseStr + chunk;            
+            }
+        }).on('end', () => {
+            responseStr = responseBufs.length > 0 ? 
+                Buffer.concat(responseBufs).toString(responseEncoding) : responseStr;
+            
+            callback(null, res.statusCode, res.headers, responseStr);
+        });
+        
+    })
+    .setTimeout(0)
+    .on('error', (error) => {
+        callback(error);
+    });
+    request.write("")
+    request.end();
+    
+
+})((error, statusCode, headers, body) => {
+    console.log('ERROR:', error); 
+    console.log('STATUS:', statusCode);
+    console.log('HEADERS:', JSON.stringify(headers));
+    console.log('BODY:', body);
+});
+
 // request Occurrence grid cells statistics 
 (function(callback) {
     'use strict';
